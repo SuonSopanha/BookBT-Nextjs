824474f987af440168344140c123f4b382dc7b08
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-05-10 18:26:42 +0700

    panha update

--- src/app/booking/form/page.jsx
+++ src/app/booking/form/page.jsx
@@ -1,6 +1,7 @@
-//Need to  fix
+
 
 const BookingForm = () => {
+
   return (
     <div className="w-full">
       <div className="h-28"></div>
@@ -155,73 +156,8 @@ const BookingForm = () => {
                               <div className="text-sm">
                                 <p>Select your seats</p>
                               </div>
-                              <div className="mt-2">
-                                <button
-                                  id="dropdownDefaultButton"
-                                  data-dropdown-toggle="dropdown"
-                                  className="inline-flex items-center rounded-lg bg-white px-2 py-2.5 text-center text-sm font-medium text-gray-400 hover:bg-yellow-200 focus:bg-white focus:outline-none focus:ring-4 dark:hover:bg-yellow-200 dark:focus:bg-yellow-200"
-                                  type="button"
-                                >
-                                  Position
-                                  <svg
-                                    className="ms-3 h-2.5 w-2.5"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 10 6"
-                                  >
-                                    <path
-                                      stroke="currentColor"
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      stroke-width="2"
-                                      d="m1 1 4 4 4-4"
-                                    />
-                                  </svg>
-                                </button>
-                                <div
-                                  id="dropdown"
-                                  className="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
-                                >
-                                  <ul
-                                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                    aria-labelledby="dropdownDefaultButton"
-                                  >
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Front
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Middle
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Back
-                                      </a>
-                                    </li>
-                                    <li>
-                                      <a
-                                        href="#"
-                                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                      >
-                                        Window
-                                      </a>
-                                    </li>
-                                  </ul>
-                                </div>
-                              </div>
+
+                  
                             </div>
                           </div>
                           <div className="flex h-44 w-full items-center justify-center">

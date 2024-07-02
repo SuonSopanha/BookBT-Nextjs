b94e9bdb7c80564c52b2eda3c6f204b606427c40
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-07-03 01:09:24 +0700

    update some UI

--- src/app/bus/[id]/page.jsx
+++ src/app/bus/[id]/page.jsx
@@ -95,7 +95,7 @@ const BusDetail = () => {
       <div className="relative">
         <div className="relative bg-black bg-opacity-40 py-10">
           <div className="mx-auto flex flex-col justify-center space-y-4 p-4 md:flex-row md:space-x-20 md:space-y-0">
-            <div className="relative rounded-lg bg-white">
+            <div className="relative bg-gradient-to-r from-white via-gray-100 to-white p-3 rounded-lg shadow-sm">
               <img
                 src={driver.photoURL}
                 className="h-80 w-full rounded-t-lg object-cover"
@@ -105,7 +105,7 @@ const BusDetail = () => {
                   {driver.firstName} {driver.lastName}
                 </p>
               </div>
-              <div className="m-4 border-b-2 border-zinc-400 pb-12">
+              <div className="m-4 border-b-2 border-zinc-400 pb-12 space-y-2">
                 <div className="ml-0.5 flex items-center">
                   <img
                     width="28"
@@ -141,7 +141,7 @@ const BusDetail = () => {
                 </div>
               </div>
 
-              <div className="m-4">
+              <div className="m-4 ">
                 <div className="ml-1 flex items-center">
                   <img
                     width="24"
@@ -164,14 +164,79 @@ const BusDetail = () => {
                   />
                 </div>
               </div>
-              <div className="flex items-center justify-center space-x-8 px-2 py-4">
-                <div className="flex items-center justify-center">
-                  <Link href="#">
-                    <button className="h-10 w-32 rounded-xl border-2 border-zinc-600 hover:bg-yellow-300">
-                      <p className="text-lg font-bold text-yellow-400 hover:text-white">
-                        Follow Taxi
-                      </p>
+              <div className="ml-2 py-8 border-t-2 border-yellow-400">
+                <div className="flex items-center justify-between mb-6 px-4">
+                  <p className="text-lg font-medium text-gray-800">
+                    7 comments
+                  </p>
+                  <ReactStars
+                    count={5}
+                    value={rating}
+                    onChange={(newRating) => setRating(newRating)}
+                    size={28}
+                    edit={true}
+                    isHalf={true}
+                    activeColor="#ffd700"
+                  />
+                </div>
+
+                <div className="flex flex-col space-y-4 px-4 mb-6">
+                  <div className="flex items-center space-x-4">
+                    <input
+                      type="text"
+                      value={feedback}
+                      onChange={(e) => setFeedback(e.target.value)}
+                      className="flex-1 h-12 rounded-lg border border-gray-300 bg-white p-3 font-medium text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                      placeholder="Add comment..."
+                    />
+                    <button
+                      onClick={handleRateDriver}
+                      className="h-12 w-24 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                    >
+                      Post
                     </button>
+                  </div>
+                </div>
+
+                <div className="space-y-4 px-4">
+                  {userRating.map((rating) => (
+                    <div
+                      key={rating.id}
+                      className="flex items-start space-x-4 bg-white bg-opacity-50 p-4 rounded-lg shadow-md border border-gray-200"
+                    >
+                      <img
+                        width="48"
+                        height="48"
+                        src={rating?.user.photoURL}
+                        alt="user-avatar"
+                        className="rounded-full"
+                      />
+                      <div className="flex-1 ml-4">
+                        <ReactStars
+                          count={5}
+                          value={rating.rating}
+                          size={16}
+                          edit={false}
+                          isHalf={true}
+                          activeColor="#ffd700"
+                        />
+                        <p className="font-semibold text-gray-800">
+                          {rating?.user.fullName}
+                        </p>
+                        <p className="text-gray-600 mt-1">
+                          {rating?.feedbackText}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="pt-4 px-4">
+                  <Link
+                    href="#"
+                    className="text-yellow-600 font-medium hover:underline"
+                  >
+                    See more
                   </Link>
                 </div>
               </div>
@@ -180,106 +245,151 @@ const BusDetail = () => {
             <div className="relative w-full sm:w-1/2 rounded-lg bg-white shadow-md">
               <div className="p-6">
                 <div className="flex items-center mb-6">
-                  <p className="text-xl font-bold text-gray-800">
+                  <p className="text-2xl font-bold text-gray-800">
                     Taxi Provider Profile
                   </p>
                 </div>
 
-                <div className="flex mb-8">
-                  <div className="flex items-center justify-center h-56 w-1/2 bg-gray-100 border-2 rounded-lg">
+                <div className="flex mb-8 bg-gradient-to-r from-white via-gray-100 to-yellow-50 p-3 rounded-lg shadow-sm">
+                  <div className="flex items-center justify-center h-56 w-1/2 bg-yellow-100 border-2 border-gray-300 rounded-lg overflow-hidden">
                     <img
                       src={service.vehiclePictureURL[0]}
                       alt="Vehicle Image"
+                      className="w-full h-full object-cover"
                     />
                   </div>
-                  <div className="w-1/2 ml-4 flex flex-col space-y-2">
+                  <div className="w-1/2 ml-6 flex flex-col space-y-4">
                     <button
                       type="button"
-                      className="mb-4 h-10 w-full rounded-lg border-2 border-blue-700 bg-gray-200 text-center font-medium text-gray-700"
+                      className="h-12 w-full rounded-lg border-2 border-yellow-400 bg-blue-50 text-yellow-500 font-semibold hover:bg-yellow-100 transition ease-in-out duration-150"
                     >
-                      <p>
-                        {service.location} - {service.destination}
-                      </p>
+                      {service.location} - {service.destination}
                     </button>
-                    <p>
-                      <strong>Category:</strong> {service.category}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Category:
+                      </strong>{" "}
+                      {service.category}
                     </p>
-                    <p>
-                      <strong>Solo Ride Option:</strong>{" "}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Solo Ride Option:
+                      </strong>{" "}
                       {service.soloRideOption ? "Yes" : "No"}
                     </p>
-                    <p>
-                      <strong>Destination:</strong> {service.destination}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Destination:
+                      </strong>{" "}
+                      {service.destination}
                     </p>
-                    <p>
-                      <strong>Location:</strong> {service.location}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Location:
+                      </strong>{" "}
+                      {service.location}
                     </p>
-                    <p>
-                      <strong>Vehicle Type:</strong> {service.vehicleType}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Vehicle Type:
+                      </strong>{" "}
+                      {service.vehicleType}
                     </p>
-                    <p>
-                      <strong>Max Seat:</strong> {service.maxSeat}
+                    <p className="text-gray-700">
+                      <strong className="font-semibold text-gray-900">
+                        Max Seat:
+                      </strong>{" "}
+                      {service.maxSeat}
                     </p>
 
                     <button
                       id="searchBook"
-                      className="relative mt-5 flex h-12 w-40 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-yellow-500 to-amber-200 text-sm font-medium text-black shadow-lg shadow-slate-500 hover:border-2 hover:border-gray-950"
+                      className="relative mt-6 flex h-12 w-40 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-yellow-400 to-amber-300 text-sm font-medium text-gray-900 shadow-lg hover:border-2 hover:border-gray-800 transition ease-in-out duration-150"
                     >
-                      <a href={"/booking/form/" + service.id}>Book Now</a>
-                      <div className="absolute right-0 mt-12 h-20 w-20 translate-x-1/2 transform rounded-full bg-yellow-500"></div>
+                      <a href={"/booking/form/" + service.id} className="z-10">
+                        Book Now
+                      </a>
+                      <div className="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 h-20 w-20 bg-yellow-400 rounded-full opacity-30"></div>
                     </button>
                   </div>
                 </div>
-
-                <div className="bg-white shadow-md rounded-lg p-6">
-                  <h1 className="text-3xl font-bold mb-6 border-b-2 pb-2">
+                <div className="mx-4 mt-1 p-6 bg-gradient-to-r from-white via-gray-100 to-white shadow-lg rounded-lg">
+                  <h1 className="text-3xl font-bold mb-6 pb-2 text-zinc-700 border-b-2 border-yellow-400">
                     Service Details
                   </h1>
 
                   <div className="mb-8">
-                    <h2 className="text-2xl font-semibold mb-4 text-gray-700">
+                    <h2 className="text-2xl font-semibold mb-4 text-yellow-600">
                       Pricing
                     </h2>
-                    <div className="space-y-2">
-                      <p className="text-gray-600 font-bold">
-                        <strong className="font-medium text-gray-800">
+                    <div className="space-y-4">
+                      <div className="flex justify-between items-center">
+                        <strong className="text-lg text-gray-800">
                           Base Fare:
-                        </strong>{" "}
-                        {pricing.baseFare} {pricing.currencyType}
-                      </p>
-                      <p className="text-gray-600 font-bold">
-                        <strong className="font-medium text-gray-800">
+                        </strong>
+                        <p className="text-lg text-gray-600 font-bold">
+                          {pricing.baseFare} {pricing.currencyType}
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center">
+                        <strong className="text-lg text-gray-800">
                           Additional Charge:
-                        </strong>{" "}
-                        {pricing.additionalCharge} {pricing.currencyType}
-                      </p>
-                      <p className="text-gray-600 font-bold">
-                        <strong className="font-medium text-gray-800">
+                        </strong>
+                        <p className="text-lg text-gray-600 font-bold">
+                          {pricing.additionalCharge} {pricing.currencyType}
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center">
+                        <strong className="text-lg text-gray-800">
                           Solo Charge:
-                        </strong>{" "}
-                        {pricing.soloCharge} {pricing.currencyType}
-                      </p>
-                      <p className="text-gray-600">
-                        <strong className="font-medium text-gray-800">
+                        </strong>
+                        <p className="text-lg text-gray-600 font-bold">
+                          {pricing.soloCharge} {pricing.currencyType}
+                        </p>
+                      </div>
+                      <div className="flex justify-between">
+                        <strong className="text-lg text-gray-800">
                           Description:
-                        </strong>{" "}
-                        {pricing.description}
-                      </p>
+                        </strong>
+                        <p className="mt-2 text-gray-600 text-sm">
+                          {pricing.description}
+                        </p>
+                      </div>
                     </div>
                   </div>
 
-                  <div className="mb-8">
-                    <h2 className="text-2xl font-semibold mb-4 text-gray-700">
-                      Avaiable Schedule
+                  <div className="mt-6">
+                    <h2 className="text-3xl font-bold mb-6 text-yellow-600 border-b-4 border-yellow-300 pb-2">
+                      Available Schedule
                     </h2>
-                    <ul className="list-disc pl-5 space-y-2">
+                    <ul className="space-y-2">
                       {schedule.map((sch) => (
-                        <li key={sch.id} className="text-gray-600">
-                          <span className="font-medium text-gray-800">
-                            {sch.dayOfWeek}:
-                          </span>{" "}
-                          {sch.departureTime.slice(0, 5)} -{" "}
-                          {sch.arrivalTime.slice(0, 5)}
+                        <li
+                          key={sch.id}
+                          className="flex items-center bg-gray-50 p-4 rounded-lg shadow-md hover:bg-gray-100 transition duration-300 ease-in-out"
+                        >
+                          <div className="w-6 h-6 flex items-center justify-center bg-yellow-300 rounded-full mr-4">
+                            <svg
+                              className="w-4 h-4 text-yellow-700"
+                              fill="none"
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zM12 7v6l4 2" />
+                            </svg>
+                          </div>
+                          <div className="flex">
+                            <p className="text-md font-medium text-gray-800">
+                              <span className="font-semibold text-gray-700">
+                                {sch.dayOfWeek}:
+                              </span>{" "}
+                              {sch.departureTime.slice(0, 5)} -{" "}
+                              {sch.arrivalTime.slice(0, 5)}
+                            </p>
+                          </div>
                         </li>
                       ))}
                     </ul>
@@ -289,71 +399,6 @@ const BusDetail = () => {
             </div>
           </div>
 
-          <div className="ml-4 py-8 md:pl-28">
-            <div>
-              <p className="pl-4 font-medium text-white">7 comments</p>
-            </div>
-            <ReactStars
-              classNames="text-center"
-              count={5}
-              value={rating}
-              onChange={(newRating) => setRating(newRating)}
-              size={28}
-              edit={true}
-              isHalf={true}
-              activeColor="#ffd700"
-            />
-            <div className="mx-auto flex w-full items-center justify-start py-4 pl-4">
-              <div className="flex space-x-4">
-                <input
-                  type="text"
-                  value={feedback}
-                  onChange={(e) => setFeedback(e.target.value)}
-                  className="h-10 w-96 rounded-lg border border-gray-400 bg-gray-100 p-3 font-medium leading-normal placeholder-gray-400 placeholder:text-white focus:bg-white focus:outline-none"
-                  placeholder="Add comment..."
-                />
-                <Link href="#" className="flex items-center justify-center">
-                  <button
-                    onClick={handleRateDriver}
-                    className="h-10 w-20 rounded-lg bg-blue-600 font-medium text-teal-50 hover:bg-yellow-200 hover:text-white"
-                  >
-                    Post
-                  </button>
-                </Link>
-              </div>
-            </div>
-            {userRating.map((rating) => (
-              <div key={rating.id} className="flex items-center p-4 pl-4">
-                <img
-                  width="48"
-                  height="48"
-                  src={rating?.user.photoURL}
-                  alt="user-male-circle"
-                />
-                <div className="ml-4 w-60 border-b-2">
-                  <ReactStars
-                    classNames="text-center"
-                    count={5}
-                    value={rating.rating}
-                   
-                    size={16}
-                    edit={false}
-                    isHalf={true}
-                    activeColor="#ffd700"
-                  />
-                  <p className="font-medium text-white">
-                    {rating?.user.fullName}
-                  </p>
-                  <p className="ml-4 text-sm text-white">
-                    {rating?.feedbackText}
-                  </p>
-                </div>
-              </div>
-            ))}
-            <div className="py-4 pl-8 font-medium text-white md:flex">
-              <Link href="#">See more</Link>
-            </div>
-          </div>
         </div>
       </div>
     </div>
